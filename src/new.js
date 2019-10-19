d2489e5a98dd906464119f14d739a59b5811ab7b
jeffinsam <jeffinsam@karunya.edu.in>
2019-10-19 15:09:34 +0530

    trying menu bar

---
+++ src/new.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.less';
+
+class Welcome extends React.Component {
+    render () {
+        return (
+            <>
+                <h1 className="header">Karunya Hacks</h1>
+                <p>WELCOME</p>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
