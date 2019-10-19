c2626be789743aab3cc62697def2349a6c144cf2
jeffinsam <jeffinsam@karunya.edu.in>
2019-10-19 14:39:35 +0530

    initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: "./build",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      { 
+        test: /\.less$/,
+        use: [ 
+          'style-loader',
+          'css-loader', 
+          'less-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:  path.resolve('./index.html'),
+    }),
+  ]
+};
